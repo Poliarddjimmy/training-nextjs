8550900fe26c22b8a69e18f5d5c2646005d16511
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-09 10:15:01 -0400

    Updated index page

--- pages/index.js
+++ pages/index.js
@@ -2,16 +2,22 @@ import { useState, useEffect } from "react";
 import Layout from "../components/layouts/layout";
 import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
+import { fetchCoursesAction } from "../redux/actions/courseAction";
 
 
 export default function Home() {
   const allCourses = useSelector(state => state.course.courses)
   const [courses, setCourses] = useState(allCourses)
 
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    allCourses && setCourse(allCourses)
+    allCourses && setCourses(allCourses)
   }, [allCourses])
 
+  useEffect(() => {
+    dispatch(fetchCoursesAction())
+  }, [])
 
   return (
     <Layout>
@@ -29,11 +35,11 @@ export default function Home() {
               <div className="stm_woo_helpbar clearfix">
                 <div className="pull-left">
                   <form role="search" method="get" className="training-search" action="">
-                    <label className="screen-reader-text" for="training-search-field-0">
+                    <label className="screen-reader-text" htmlFor="training-search-field-0">
                       Search for:
                       </label>
-                    <input type="search" id="training-search-field-0" className="search-field" placeholder="Search the Courses" value="" name="s" title="Search for:" />
-                    <input className="heading_font" type="submit" value="Search" /> <input type="hidden" name="post_type" value="product" />
+                    <input type="search" id="training-search-field-0" className="search-field" placeholder="Search the Courses" title="Search for:" />
+                    <input className="heading_font" type="submit" />
                   </form>
                 </div>
                 <div className="pull-right xs-right-help">
@@ -53,47 +59,53 @@ export default function Home() {
               <div className="stm_archive_product_inner_grid_content">
                 <ul className="stm-courses row list-unstyled">
 
-                  {courses ?
-                    courses.map(course =>
-                      <div className="stm_woo_archive_view_type_list">
-                        <li className="col-md-12 course-col-list first post-490 product type-product status-publish has-post-thumbnail product_cat-business-management product_cat-logical-thinking  instock sale featured shipping-taxable purchasable product-type-simple">
-                          <div className="stm_archive_product_inner_unit heading_font">
-                            <div className="stm_archive_product_inner_unit_centered clearfix">
-                              <div className="stm_featured_product_image_list">
-                                
-                                <Link href="/courses/slug" title={`View course - ${course.title}`}>
-                                  <img width="300" height="225" src="/images/img.jpg" className="img-responsive wp-post-image" alt="" />
-                                </Link>
-                              </div>
-                              <div className="stm_products_archive_body_list">
-                                <h2 className="title"> {course.title} </h2>
-                                <div className="clearfix stm_product_meta_unit">
-                                  <div className="clearfix"></div>
-                                  <div className="pull-right sm-xs-pull-left">
-                                    <div className="stm_featured_product_stock d-flex aligh-items-center justify-content-center">
-                                      <i className="fa-icon-stm_icon_users"></i>
-                                      <span className="">163 Students</span></div>
-                                  </div>
-                                </div>
-                                <div className="stm_archive_product_exceprt normal_font">
-                                  <p>{course.desctiption}</p>
-                                </div>
-                                <div className="see_more h5"> <Link href={`/courses/${course.slug}`}>Seemore</Link></div>
-                              </div>
+
+                  {courses?.map(course =>
+                    <div key={course.id} className="stm_woo_archive_view_type_list">
+                      <li className="col-md-12 course-col-list first post-490 product type-product status-publish has-post-thumbnail product_cat-business-management product_cat-logical-thinking  instock sale featured shipping-taxable purchasable product-type-simple">
+                        <div className="stm_archive_product_inner_unit heading_font">
+                          <div className="stm_archive_product_inner_unit_centered clearfix">
+                            <div className="stm_featured_product_image_list">
+
+                              <Link href={`/courses/${course.slug}`} title={`View course - ${course.name}`}>
+                                <img width="300" height="225" src={course.picture || `/images/courset.jpeg`} className="img-responsive wp-post-image cursor-pointer" alt="" />
+                              </Link>
                             </div>
-                            <div className="multiseparator"></div>
+                            <div className="stm_products_archive_body_list">
+                              <Link href={`/courses/${course.slug}`} title={`View course - ${course.name}`}>
+                                <h2 className="title cursor-pointer">
+                                  {course.name}
+                                </h2>
+                              </Link>
+                              <div className="clearfix stm_product_meta_unit">
+                                <div className="clearfix"></div>
+                                <div className="pull-right sm-xs-pull-left">
+                                  <div className="stm_featured_product_stock d-flex aligh-items-center justify-content-center">
+                                    <i className="fa-icon-stm_icon_users"></i>
+                                    <span className="">163 Students</span></div>
+                                </div>
+                              </div>
+                              <div className="stm_archive_product_exceprt normal_font">
+                                <p>{course.description}</p>
+                              </div>
+                              <div className="see_more h5 cursor-pointer"> <Link href={`/courses/${course.slug}`}>Seemore</Link></div>
+                            </div>
                           </div>
-                        </li>
-                      </div>
-                    )
-                    :
-                    <div className="stm_woo_archive_view_type_list">
+                          <div className="multiseparator"></div>
+                        </div>
+                      </li>
+                    </div>
+                  )}
+                  {
+                    courses?.length === 0 &&
+                    <div div div className="stm_woo_archive_view_type_list">
                       <li className="col-md-12 course-col-list first post-490 product type-product status-publish has-post-thumbnail product_cat-business-management product_cat-logical-thinking  instock sale featured shipping-taxable purchasable product-type-simple">
                         No courses found
-                    </li>
-                    </div> 
-
+                      </li>
+                    </div>
                   }
+
+
                 </ul>
                 <div className="multiseparator list"></div>
               </div>
@@ -104,13 +116,13 @@ export default function Home() {
               <aside id="archives-3" className="widget widget_archive">
                 <div className="widget_title">
                   <h3>Must popular course</h3>
-                </div> 
+                </div>
 
-              </aside>    
+              </aside>
             </div>
           </div>
         </div>
       </div>
-    </Layout>
+    </Layout >
   )
 }
