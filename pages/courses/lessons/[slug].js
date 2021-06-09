b1f6509e5fdc5090f35902830a010ee312577112
Poliard Djimmy <poliarddjimmy@gmail.com>
2021-06-09 10:16:02 -0400

    Import props form page

--- pages/courses/lessons/[slug].js
+++ pages/courses/lessons/[slug].js
@@ -1,14 +1,28 @@
+import { useState, useEffect } from "react";
 import LessonLayout from "../../../components/layouts/LessonLayout"
+import { useDispatch, useSelector } from "react-redux";
+import { showLessonAction } from "../../../redux/actions/lessonAction";
+import { useRouter } from "next/router";
 
 const Lesson = () => {
+
+  const lesson = useSelector(state => state.lesson.lesson)
+
+  const dispatch = useDispatch()
+  const router = useRouter()
+
+  useEffect(() => {
+    router.query?.slug && dispatch(showLessonAction(router.query?.slug))
+  }, [router.query?.slug])
+
   return (
-    <LessonLayout>
+    <LessonLayout next={lesson?.next_lesson} previous={lesson?.previous_lesson}>
       <div className="p-4 d-flex justify-content-between align-items-center">
         <div>
-          <i class="bg-dark p-3 text-white rounded-circle fa fa-list-ul fa-1x" aria-hidden="true"></i>
+          <i className="bg-dark p-3 text-white rounded-circle fa fa-list-ul fa-1x" ></i>
         </div>
         <div>
-          <i class="bg-dark p-3 text-white rounded-circle fa fa-question fa-1x" aria-hidden="true"></i>
+          <i className="bg-dark p-3 text-white rounded-circle fa fa-question fa-1x" ></i>
         </div>
       </div>
 
@@ -16,14 +30,12 @@ const Lesson = () => {
         <div className="row">
           <div className="d-flex align-items-center justify-content-center">
             <div className="col-lg-8 col-md-12">
-              <h2 style={{ fontFamily: "Montserrat", color: "#273044" }}>Introduction to MasterStudy Theme</h2>
-              <h1 style={{ fontFamily: "Montserrat" }}>General Settings</h1>
+              <h2 className="text-capitalize" style={{ fontFamily: "Montserrat", color: "#273044" }}>{lesson?.chapter.title}</h2>
+              <h1 className="text-capitalize" style={{ fontFamily: "Montserrat" }}> {lesson?.title} </h1>
               <div className="mt-5" style={{ fontFamily: "Open Sans", fontSize: "18px" }}>
-                <p>
-                  Masterstudy LMS theme and plugin open unlimited opportunities for their users. Extended functionality,
-                  simplicity, and accuracy provide customers an enjoyable experience. Both products were developed in a way
-                  they could ensure a convenient use equally for admins, instructors, and students.
-                </p>
+                <p>{lesson?.description}</p>
+                <hr />
+                <p>{lesson?.content}</p>
               </div>
             </div>
           </div>
